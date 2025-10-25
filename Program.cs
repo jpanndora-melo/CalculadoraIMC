5b154ab9406ecd975a25795075e7e2def83d67cb
Jessica <jpanndora@gmail.com>
2025-10-24 21:46:30 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿double peso;
+double altura;
+double imc;
+
+string diagnostico;
+
+Console.WriteLine("-- Calculadora de IMC --\n");
+
+Console.Write("Digite seu peso em kg...:");
+peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite sua altura em m..:");
+altura = Convert.ToDouble(Console.ReadLine());
+
+imc = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"\nSeu IMC é {imc:F2}\n");
+if (imc < 18.5)
+{
+    diagnostico = "Abaixo do peso";
+}
+else if (imc < 24.9)
+{
+    diagnostico = "Peso normal";
+}
+else if (imc < 29.9)
+{
+    diagnostico = "Sobrepeso";
+}
+else
+{
+    diagnostico = "Obesidade";
+}
+
